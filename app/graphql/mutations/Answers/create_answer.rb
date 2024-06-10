2a7fbb37337c9ed24701beb0689735ef54c31af3
Your Name <alexnunessilva006@gmail.com>
2024-06-09 21:01:05 -0300

    otimização da create answer e tratamento de erros

--- app/graphql/mutations/Answers/create_answer.rb
+++ app/graphql/mutations/Answers/create_answer.rb
@@ -2,27 +2,20 @@ module Mutations
   module Answers
     class CreateAnswer < BaseMutation
       argument :answers, [Types::Arguments::CreateAnswerArguments], required: true
-    
+      argument :form_id, ID, required: true
+
       field :answers, [Types::AnswerType], null: true
       field :errors, [String], null: true
+
       def resolve(args)
-        user_id = context[:current_user].id unless context[:current_user].nil?
 
-        answers = Answer::Creator.call(args, user_id)
+        result = Answer::Creator.call(args, args[:form_id], context[:current_user])
 
-        saved_answers = []
-        errors = []
-        answers.each_with_index do |answer, idx|
-          if answer.save
-             saved_answers << answer
-          else
-            answer_errors = answer.errors.full_messages.join(", ")
-            answer_errors = "answer #{idx+1}: #{answer_errors}"
-            errors << answer_errors
-          end
+        if result[:errors].blank?
+          {answers: result[:answers]}
+        else
+          {errors: result[:errors]}
         end
-        {answers: saved_answers, errors: errors}
-
       end
     end
   end
--- app/services/answer/creator.rb
+++ app/services/answer/creator.rb
@@ -1,23 +1,49 @@
 class Answer::Creator < ApplicationServices
 
-  def initialize(arguments, user_id=nil)
+  def initialize(arguments, form_id, user)
+    user_id = user.id unless user.nil?
+
     @answers = arguments[:answers].map do |answer|
       answer = answer.to_h.merge(user_id: user_id)
     end
-    
+
+    @form = Form.find(form_id)
+  
+    @errors = []
+
+  rescue ActiveRecord::RecordNotFound => e
+    raise GraphQL::ExecutionError, e
   end
 
   def call
     create_answer
+    {answers: @answers, errors: @errors}
   end
 
-  def create_answer
-    answers = []
-    ActiveRecord::Base.transaction do
-      @answers.map do |answer|
-        answers << answer = Answer.new(answer)
+  def create_answer 
+    @answers.each_with_index do |answer, idx|
+      answer = Answer.new(answer)
+      @answers[idx] = answer
+      answer.valid?
+
+      unless @form.questions.exists?(id: answer.question_id)
+        answer.errors.add(:base, "question not found on form")
+      end
+
+      if answer.errors.any?
+        error = answer.errors.full_messages.join(", ")
+        @errors << "answer #{idx+1}: #{error}"
       end
     end
-    answers
+    
+
+    if @errors.blank? 
+      ActiveRecord::Base.transaction do
+        @answers.each(&:save!)
+      end
+    end
+    
+  rescue ActiveRecord::RecordNotFound => e
+    raise GraphQL::ExecutionError, e
   end
 end
